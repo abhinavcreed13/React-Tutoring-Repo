daca928613fbff8391c84095f44638d6b856ce12
abhinavcreed13 <abhinavcs13@gmail.com>
2020-09-18 20:06:02 +1000

    Updating App.js

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Javascript object
+const welcome = {
+  greeting: 'Hey',
+  title: 'React'
+}
+
+// Function
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
+    // React JSX -> babel -> React.functions -> HTML
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {welcome.greeting} {getTitle(welcome.title)}
+      </h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
     </div>
   );
 }
