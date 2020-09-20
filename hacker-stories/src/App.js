260c68936f5846e1b1d039d46324d7ef0cb4800f
abhinavcreed13 <abhinavcs13@gmail.com>
2020-09-20 19:21:12 +1000

    update

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -7,27 +7,6 @@ const welcome = {
   title: 'React'
 }
 
-// stories
-// javascript list/array
-const list = [
-  {
-    title: 'React',
-    url : 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url : 'https://redux.js.org/',
-    author: 'Dan Abramov',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  },
-]
-
 // Function
 // Not a component - it is not returning JSX
 function getTitle(title) {
@@ -35,10 +14,10 @@ function getTitle(title) {
 }
 
 // List Component
-const List = () => {
+const List = (props) => {
     // return? -> JSX
     return (
-      list.map(item => (
+      props.data.map(item => (
                 <div key={item.objectID}>
                   <span>
                     <a href={item.url}>{item.title}</a>
@@ -51,12 +30,49 @@ const List = () => {
     );
 }
 
+// Just -> Function
+// return JSX
+const Search = (props) => {
+  return (
+      <div>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" onChange={props.onSearch}/>
+      </div>
+  );
+}
+
 // Functional component
 const App = () => {
 
-  const handleChange = event => {
-    console.log(event.target.value);
+  // array destructuring
+  // useState React hook
+  const [searchTerm, setSearchTerm] = React.useState(''); 
+
+  // callback handler
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
   };  
+
+  // stories
+  // javascript list/array
+  const stories = [
+    {
+      title: 'React',
+      url : 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: 'Redux',
+      url : 'https://redux.js.org/',
+      author: 'Dan Abramov',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    },
+  ]
 
   return (
     // React JSX -> babel -> React.functions -> HTML
@@ -64,12 +80,14 @@ const App = () => {
       <h1>
         My Hacker Stories
       </h1>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange}/>
+      
+      <Search onSearch={handleSearch}/>
+      <p>
+          Searching for <strong>{searchTerm}</strong>
+      </p>
+      <hr />
 
-      <hr/>
-
-      <List/>
+      <List data={stories}/>
     </div>
   );
 }
